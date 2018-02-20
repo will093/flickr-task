0e3ed933d53db6515b10ca3e41c6f7b2b7a54f44
Will <willst.willst@gmail.com>
2018-02-20 08:29:31 +0000

    Implement feed list component

--- src/app/components/flickr-feed-list/flickr-feed-list.component.ts
+++ src/app/components/flickr-feed-list/flickr-feed-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FlickrFeedApiService } from '../../services/flickr-feed-api.service';
 import { FlickrFeedData } from '../../models/flickr-feed-data';
+import { FlickrFeedApiService } from '../../services/flickr-feed-api.service';
+import { FlickrFeedItemData } from '../../models/flickr-feed-item-data';
 
 @Component({
   selector: 'app-flickr-feed-list',
@@ -11,12 +12,16 @@ import { FlickrFeedData } from '../../models/flickr-feed-data';
 export class FlickrFeedListComponent implements OnInit {
 
   flickrFeedData: FlickrFeedData;
+  flickrFeedItems: FlickrFeedItemData[];
 
   constructor(private flickrFeedApiService: FlickrFeedApiService) { }
 
   ngOnInit() {
+    this.flickrFeedItems = [];
+    
     this.flickrFeedApiService.getFeed('potato').subscribe(data => {
       this.flickrFeedData = data;
+      this.flickrFeedItems = data.items;
     })
   }
 
--- src/app/models/flickr-feed-data.ts
+++ src/app/models/flickr-feed-data.ts
@@ -1,7 +1,10 @@
+import { FlickrFeedItemData } from "./flickr-feed-item-data";
+
 export interface FlickrFeedData {
     title: string;
     link: string;
     description: string;
     modified: string;
     generator: string;
+    items: FlickrFeedItemData[];
 }
